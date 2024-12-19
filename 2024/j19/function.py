342ac32404c1ab8bc9fc70983b8146d21d72f931
Robin MENESTRET <robin.menestret@cea.fr>
2024-12-19 20:55:24 +0100

    j19p2

--- 2024/j19/function.py
+++ 2024/j19/function.py
@@ -2,18 +2,21 @@ def parser():
     file = open("2024/j19/input.txt").read().split("\n")
     return file[0].split(", "), file[2:]
 
-def rec(model, available_towels):
+def rec(current_letter, model, computed_values, available_towels):
+    model_temp = model[current_letter:]
     matches = []
-    for i in range(len(model)+1):
-        if model[:i] in available_towels :
-            #print(model[:i])
-            matches.append(i)
-    for i in matches :
-        if model[i:]=="":
-            return True
-        valid = rec(model[i:], available_towels)
-        if valid:
-            return True
+    for last_letter in range(len(model_temp)):
+        if model_temp[:last_letter+1] in available_towels : 
+            matches.append(last_letter+1)
+    for last_letter in matches :
+        if computed_values[current_letter+last_letter] != 0 :
+            computed_values[current_letter] += computed_values[current_letter+last_letter]
+        elif len(model_temp) == last_letter :
+            computed_values[current_letter] += 1
+        else :
+            computed_values = rec(current_letter+last_letter, model, computed_values, available_towels)
+            computed_values[current_letter]+=computed_values[current_letter+last_letter]
+    return computed_values
 
     
 
@@ -21,25 +24,12 @@ def part1():
     available_towels, models_todo = parser()
     count = 0
     for model in models_todo:
-        if rec(model, available_towels):
-            print(model)
-            count +=1
+        computed_values = [0]*(len(model)+1)
+        computed_values = rec(0, model, computed_values, available_towels)
+        count += computed_values[0]
+        #print(model)
         #quit() if input() == "q" else True
         
-    print(count)
-
-def part1_man():
-    available_towels, models_todo = parser()
-    count = 0
-    for model in models_todo :
-        if model[:2] == "ur":
-            print(model)
-            # if model[3:5] not in ["ug", "br", "gr"]:
-            #     print(model)
-            # else :
-            #     print("a test")
-        else :
-            count += 1
     print(count)
 
 part1()
